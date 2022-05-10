7869faff4f943057c88e76adc4ec728326ebb33e
Cam Lindsay <cameronlindsay@Cams-MacBook-Pro.local>
2022-05-10 09:31:00 +0100

    minor change fixes client messages auth error

---
+++ functions/getLatLong.js
@@ -0,0 +1,17 @@
+import { geoKey } from '../firebase2';
+import Geocoder from 'react-native-geocoding';
+
+const getLatLong = async (postcode) => {
+  let location;
+  Geocoder.init(geoKey);
+
+  return Geocoder.from(postcode)
+    .then((json) => {
+      location = json.results[0].geometry.location;
+      console.log(location);
+      return location;
+    })
+    .catch((error) => console.warn(error));
+};
+
+export default getLatLong;
